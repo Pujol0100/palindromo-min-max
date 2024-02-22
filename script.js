61d29cda6b42bd76d70c96b31d94411dabfbbb7a
Vinicius Pujol A. M. Piccirilli <vinicius.pujol@yahoo.com>
2024-02-22 11:01:42 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    
+	str = str.toLowerCase();
+	str = str.split(" ");
+	str = str.join("");
+	
+	for (let i = 0 ; i <= str.length ; i++) {
+		
+		if (str[i] !== str[str.length - 1 - i]) {
+			return false;
+		}
+	}
+	
+	return true
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+	
+	let arrMax = arr[0];
+	let arrMin = arr[arr.length - 1];
+	
+	for (let i = 0 ; i <= arr.length ; i++) {
+		
+		if (arr[i] > arrMax) {
+			
+			arrMax = arr[i];
+		} else {
+			
+			arrMax = arrMax;
+		}
+		
+		if (arr[i] < arrMin) {
+			
+			arrMin = arr[i]
+		} else {
+			
+			arrMin = arrMin
+		}
+	}
+	
+	let resultArr = [arrMin, arrMax]
+	return resultArr
+
 }
